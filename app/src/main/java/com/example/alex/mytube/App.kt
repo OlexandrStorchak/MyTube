afa5d7223964b6aa4130288ead72ada5518740c3
AlexSt <alexsstorchak@gmail.com>
2018-04-21 23:23:38 +0300

    Some Room instance fixed App start without crash Set correct time in os, for git log

--- app/src/main/java/com/example/alex/mytube/App.kt
+++ app/src/main/java/com/example/alex/mytube/App.kt
@@ -5,6 +5,7 @@ import android.app.Application
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-       // MyRoomDB.getInstance(this)
+        //MyRoomDB.getInstance(this)
+
     }
 }
